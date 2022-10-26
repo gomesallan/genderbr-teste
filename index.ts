daab789057b3c4a00b553ae02d543466756222e7
Allan Gomes <gomesallan>
2022-10-26 08:30:21 -0300

    init

---
+++ index.ts
@@ -0,0 +1,15 @@
+// module.export ={}
+import { getGenderByName } from 'br-gender';
+
+async function main(name:string) {  
+    const gender = await getGenderByName(name, { percentage: true });
+    // console.log(gender)
+    return gender;
+}
+main('Douglas')
+    .then(text => {
+        console.log(text);
+    })
+    .catch(err => {
+        // Deal with the fact the chain failed
+    });
