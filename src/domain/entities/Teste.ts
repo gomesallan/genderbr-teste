4cc8222abfa544d6d4b70c7930318b422458d211
lizzedev <lizzeaplicativo@gmail.com>
2022-10-27 10:53:13 -0300

    teste

--- src/domain/entities/Teste.ts
+++ src/domain/entities/Teste.ts
@@ -1,6 +1,7 @@
 import { Entity } from '../../core/domain/Entity';
 import {Request, Response} from 'express';
 import { getGenderByName } from 'br-gender';
+import csv from '@fast-csv/parse'
 
 export type TesteProps = {
     param: string
@@ -13,11 +14,21 @@ export class Teste extends Entity<TesteProps> {
 
   static async run(req:Request,res:Response){
 
-    try{  
-        const { param } = req.params;
-        const gender = await getGenderByName(param, { percentage: true });
+    try{
+        const filem = `../CNPJ_MACHOS.csv`;
+        const filef = `../CNPJ_FEMEAS.csv`;
+       // const { param } = req.params;
+       // const gender = await getGenderByName(param, { percentage: false });/////////////
 
-        return res.status(200).send(gender); 
+       let rows = [];
+        csv.parseFile("../../../CNPJ.csv")
+        .on('error', error => console.error(error))
+        .on('data', (row)  => {
+            rows.push(JSON.stringify(row[1]))
+            console.log(JSON.stringify(row[1]))
+            //console.log(JSON.stringify(row[30]).replace(/[^0-9\.]+/g, ''))
+        })
+        return res.download(filem);
     }catch(e){
         console.log(e)
         return res.status(500).send({error:e})
